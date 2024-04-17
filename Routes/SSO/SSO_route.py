44516d4c2c22cd70ff3514051fcac942985a0514
Daniel V <danielvelezvasco1235@gmail.com>
2024-04-16 20:55:23 -0500

    Updated route

--- Routes/SSO/SSO_route.py
+++ Routes/SSO/SSO_route.py
@@ -6,6 +6,6 @@ from EVE_SSO import refresh_token as rt
 sso_token_route = APIRouter(prefix = "/SSO")
 
 
-@sso_token_route.post("/refresh_token", include_in_schema = False)
-async def refresh_token(id: str):
-    return rt(id)
+@sso_token_route.post("/refresh_token")
+async def refresh_token(uuid: str):
+    return rt(uuid)
