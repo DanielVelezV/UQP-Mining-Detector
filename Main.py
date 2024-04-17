9da5aff3840f430a5e9cf0d475b490fc6b28e3ad
Daniel V <danielvelezvasco1235@gmail.com>
2024-04-16 20:51:46 -0500

    SSO login refactor

--- Main.py
+++ Main.py
@@ -2,6 +2,10 @@ from EVE_SSO import create_sso_url, check_state, get_auth, check_token_signature
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 import Routes
+import Query
+from Models import PydanticModels as Models
+import EVE_ESI
+import time
 
 app = FastAPI(title = "EVE SSO Auth Programn")
 
@@ -20,16 +24,42 @@ async def sso(request: Request, code: str, state: str):
     if not check_state(state):
         return "State not allowed"
     
-    response = get_auth(code)
+    try:
 
-    # Checking the token signature. Security Handler
-    contents = check_token_signature(response.json()["access_token"])
+        auth = Models.CharAUth.model_validate(get_auth(code).json())
 
-    if not contents:
-        return "Error decrypting your Auth Code. Communicate with ElWarriorcito"
+        # Checking the token signature. Security Handler
+        contents = Models.CharDecryptedAuth.model_validate(check_token_signature(auth.access_token))
+    except:
+        return {"Error" : "There was an erro parsing the data"}
+    
+    roles = []
 
-    # Saving the data
+    if "esi-characters.read_corporation_roles.v1" in contents.scp:
+        roles = EVE_ESI.get_roles(contents.sub.split(":")[2], auth.access_token)["roles"]
 
-    save_data(contents, response.json())
 
+    corp_id  = EVE_ESI.get_corporation_id(contents.sub.split(":")[2], auth.access_token)
+
+    corp_info = EVE_ESI.get_corp_info(corp_id)
+
+    playerModel = Models.CharDataModel(
+        CharHash = contents.owner,
+        CharName = contents.name,
+        CharID = contents.sub.split(":")[2],
+        AuthData = Models.AuthDataModel(
+            Access_Token = auth.access_token,
+            Refresh_Access_Token = auth.refresh_token,
+            Last_Refresh_Milis = round(time.time()),
+            Scopes = contents.scp
+        ),
+        CorpInfo = Models.CorpInfoModel(
+            CorpId = corp_id,
+            CorpName = corp_info["name"],
+            Roles = roles
+        )
+    )
+    
+    Query.UQPData.insert_user(playerModel)
+    
     return RedirectResponse("https://www.eveonline.com")
